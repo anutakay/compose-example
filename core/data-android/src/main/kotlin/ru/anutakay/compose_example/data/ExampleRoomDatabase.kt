6abe5e6d6b62c8f5d242dc0ac06d6b64613fe8f1
Anna Kaygorodova <akaygorodova@issart.com>
2022-11-07 22:01:53 +0700

    move modules into folders

---
+++ core/data-android/src/main/kotlin/ru/anutakay/compose_example/data/ExampleRoomDatabase.kt
@@ -0,0 +1,16 @@
+package ru.anutakay.compose_example.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.anutakay.compose_example.data.entities.DbActivityNote
+import ru.anutakay.compose_example.data.entities.DbEmotionNote
+
+@Database(
+    entities = [
+        DbActivityNote::class,
+        DbEmotionNote::class
+    ],
+    version = 2
+)
+//@TypeConverters(ExampleTypeConverters::class)
+abstract class ExampleRoomDatabase : RoomDatabase(), ExampleDatabase
